4cb4ac67ae9ca92dee6d3b268e03359ca3e462e1
Cheef44 <cheef44.prog@gmail.com>
2025-07-20 11:22:27 +0400

    feature(setup): реализация запуска программы через setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from scripts.main import LogInApp
+from PyQt6.QtWidgets import QApplication
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LogInApp()
+    window.show()
+    sys.exit(app.exec())

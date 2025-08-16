5e78bf82c4aa0406722bc6117723ab0e72709ba8
Cheef44 <cheef44.prog@gmail.com>
2025-08-16 20:17:13 +0300

    feature(main): реализация функции взаимодействия интерфейса с функцией входа в программу

--- scripts/main.py
+++ scripts/main.py
@@ -8,9 +8,16 @@ class LogInApp(Ui_log_in, QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.registration.clicked.connect(self.registration_button)
+        self.log_in_2.clicked.connect(self.log_in_button)
         
     #Взаимодействие функции регистрации с интерфейсом через API
     def registration_button(self):
         if self.password_input_1.text() == self.password_input_2.text():
             if API.registration_api(self, user_name=self.login_input.text(), user_password=self.password_input_1.text()):
+                pass
+    
+    #Взаимодействие функции входа в программу с интерфейсом через API
+    def log_in_button(self):
+        if self.password_input_1.text() == self.password_input_2.text():
+            if API.log_in_api(self, user_name=self.login_input.text(), user_password=self.password_input_1.text()):
                 pass

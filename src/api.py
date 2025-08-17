07e24a5ac0469f8fe829f9a7868b998829320888
Cheef44 <cheef44.prog@gmail.com>
2025-08-17 22:07:39 +0300

    feature(api): вызов функции получения данных пользователя и функции создания таблицы паролей

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.abspath('../src'))
 
 from src.log_in import LogIn
+from src.db import DataBase
 
 #Класс API слоя между логикой и интерфейсом
 class API:
@@ -16,3 +17,11 @@ class API:
     #Вызов функции входа
     def log_in_api(self, user_name:str, user_password:str):
         return LogIn(user_name, user_password).log_in()
+    
+    #Вызов функции получения данных пользователя
+    def data_user_api(self):
+        return DataBase().retrieve_data()
+    
+    #Вызов функции создания таблицы паролей
+    def add_password(self, name:str=None, name_sit:str=None, login:str=None, mail:bytes=None, password:bytes=None):
+        return DataBase.create_passwords_db(self, name, name_sit, login, mail, password)

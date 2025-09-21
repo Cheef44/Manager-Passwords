644aea443b1f57b6046ff6bfdcf17f27f93f243d
Cheef44 <cheef44.prog@gmail.com>
2025-09-21 23:25:50 +0300

    featur(api): реализация вызова функции генерации ключей и шифрования данных в api слой

--- src/api.py
+++ src/api.py
@@ -4,6 +4,8 @@ sys.path.append(os.path.abspath('../src'))
 
 from src.log_in import LogIn
 from src.db import DataBase
+from src.generation_keys import Keys
+from src.encryption import EncryptionText
 
 #Класс API слоя между логикой и интерфейсом
 class API:
@@ -29,3 +31,11 @@ class API:
     #Вызов функции создания таблицы паролей
     def add_password(self, name:str=None, name_sit:str=None, login:str=None, mail:bytes=None, password:bytes=None):
         return DataBase.create_passwords_db(self, name, name_sit, login, mail, password)
+    
+    #Вызов функции генерации ключей шифрования
+    def keys_generation(self):
+        Keys().run()
+    
+    #Вызов функции шифрования текста    
+    def encryption_data(self, data:str):
+        return EncryptionText(data).encryption_text()

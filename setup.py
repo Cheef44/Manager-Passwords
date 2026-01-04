b755ce42396c8f178176e72a7cf887e6dc759e41
Cheef44 <cheef44.prog@gmail.com>
2026-01-04 16:27:58 +0300

    fix(setup): исправление завершения программы с учетом очистки кэша

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from scripts.main import LogInApp
 from PySide6.QtWidgets import QApplication
 import sys
+from src import api
+
+def on_exit():
+    api.API.del_password_api(None,name_service="PASSWORD_LOG_IN")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = LogInApp()
     window.show()
+    app.aboutToQuit.connect(on_exit)
     sys.exit(app.exec())
+    print(api.API.get_password_cache_api(None, "PASSWORD_LOG_IN"))
